f49f69590983d4266a48393aae9dc72186711c82
Shubham Shivade <shubhamshivade123@gmail.com>
2024-10-30 23:57:23 +0530

    updting the bmi calculator app

--- Basic_Web_Apps/2-BMICalculator/script.js
+++ Basic_Web_Apps/2-BMICalculator/script.js
@@ -9,6 +9,7 @@ form.addEventListener('submit', function (e) {
     const weight = parseInt(document.querySelector('#weight').value);
     const results = document.querySelector("#results");
 
+
     if (height === '' || height < 0 || isNaN(height)) {
         results.innerHTML = `Pleaes give valid height ${height}`;
     } else if (weight === '' || weight < 0 || isNaN(weight)) {
@@ -17,6 +18,20 @@ form.addEventListener('submit', function (e) {
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
         // show the results
         results.innerHTML = `<strong><span>${bmi}</span></strong>`;
+        console.log(bmi)
+
+        if (bmi < 18.6) {
+            results.innerHTML = `<strong><span>${bmi} Under Weight</span></strong>`;
+            
+        } else if (bmi > 18.6 || bmi < 24.9) {
+            results.innerHTML = `<strong><span>${bmi} Normal Range</span></strong>`;
+            
+        } else {
+            results.innerHTML = `<strong><span>${bmi} Overweight</span></strong>`;
+            
+        }
+
     }
+    
 
 });
